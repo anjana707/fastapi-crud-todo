7751cf3feed99dfd2582c0ea2339119d81464d0d
Anjana Dissanayake <dmadnotdmad@gmail.com>
2026-07-27 14:51:24 +0000

    Stage 3: update and delete with SQL

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
-from sqlmodel import Field, Session, SQLModel, create_engine
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 #Initialize the FastAPI app
 app = FastAPI()
@@ -11,6 +11,17 @@ tasks = [
     {"id": 2, "title": "Task 2", "done": True},
     {"id": 3, "title": "Task 3", "done": False},
 ]
+
+class Task(SQLModel, table = True): # creates class "Tasks" consisting of three columns: id, title, done 
+    id: int | None = Field(default=None, primary_key = True)
+    title: str
+    done: bool = False
+
+sqlite_file_name = "tasks.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}" #url for database 
+engine = create_engine(sqlite_url) # create engine
+
+
 # Define temp root endpoint
 @app.get("/")   # what happens when a client sends a GET request to the root endpoint
 def real_root():
@@ -42,46 +53,47 @@ def get_task(task_id: int):
         return task # Return the matching task
    
 @app.post("/tasks", status_code=201)  # what happens when a client sends a POST request to the /tasks endpoint, sets the status code to 201 (Created)
-def create_task(task: dict):
+def create_task(task: Task):
     #1. Validate the input data
-    if "title" not in task:
+    if not task.title or task.title.strip() == "":
         return JSONResponse(status_code=400, content={"error": "Task must have a title"})  # Return an error message if the input data is invalid
     
-    #2. Process the input data and create a new task
-    task_id = len(tasks) + 1  # Generate a new task ID
-    task["id"] = task_id  # Assign the new ID to the task
-    task["done"] = False  # Set the done status to False by default
-    tasks.append(task)  # Add the new task to the list of tasks
-    return task  # Return the newly created task
+    #2. Save to database
+    with Session(engine) as session:
+        new_task = Task(title=task.title.strip(), done=False)
+        session.add(new_task) # adds new task to database session
+        session.commit() #writes task direcrly to tasks.db
+        session.refresh(new_task)  # Gets the database-assigned ID
+        return new_task
 
 @app.put("/tasks/{task_id}")  # what happens when a client sends a PUT request to the /tasks/{task_id} endpoint
-def update_task(task_id: int, updated_task: dict):
-    for task in tasks:  # Iterate through the list of tasks
-        if task["id"] == task_id:  # Check if the task ID matches the requested ID
-            task.update(updated_task)  # Update the task with the new data
-            return task  # Return the updated task
-    if not any(task["id"] == task_id for task in tasks):  # Check if the task ID does not exist in the list of tasks
-        return JSONResponse(status_code=404, content={"error": f"Unkown ID"})  # Return an error message if no matching task is found
-    
-    return JSONResponse(status_code=400, content={"error": f"Task {task_id} not found"})  # Return an error message if no matching task is found
+def update_task(task_id: int, updated_task: Task):
+    if updated_task.title is not None and updated_task.title.strip() == "":
+        return JSONResponse(status_code=400, content={"error": "Title cannot be empty"})
+    with Session(engine) as session:
+        db_task = session.get(Task, task_id)
+        if not db_task:
+            return JSONResponse(status_code=404, content= {"error": "Task not found"})
+        
+        #update attributes
+        db_task.title = updated_task.title.strip()
+        db_task.done = updated_task.done
+        session.add(db_task)
+        session.commit()
+        session.refresh(db_task)
+        return db_task
 
 @app.delete("/tasks/{task_id}")  # what happens when a client sends a DELETE request to the /tasks/{task_id} endpoint
 def delete_task(task_id: int):
-    for task in tasks: 
-        if task["id"] == task_id:
-            tasks.remove(task)
-            return JSONResponse(status_code=204, content={"message": f"Task {task_id} deleted"})  # Return a success message if the task is deleted
-    return JSONResponse(status_code=404, content={"error": f"Task {task_id} not found"})  # Return an error message if the task is not found
-
-class Task(SQLModel, table = True): # three columnds: id, title, done 
-    id: int | None = Field(default=None, primary_key = True)
-    title: str
-    done: bool = False
-
-sqlite_file_name = "tasks.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}" #url for database 
-engine = create_engine(sqlite_url) # create engine
-
+    with Session(engine) as session:
+        db_task = session.get(Task, task_id)
+        if not db_task:
+            return JSONResponse(status_code=404, content={"error": "Task not found"})
+        # if valid input for delete
+        session.delete(db_task)
+        session.commit()
+        return JSONResponse(status_code=204, content={"message": f"Task deleted"})  # Return a success message if the task is deleted
+    
 #Create Database on Startup 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
